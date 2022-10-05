22ba097f8f17ca06bb586dcd8ae017e7de020b6e
Dobriiii <dilev8g@gmail.com>
2022-10-05 18:28:37 +0300

    Updates 05.10.2022

--- ReservationManagment/Program.cs
+++ ReservationManagment/Program.cs
@@ -16,7 +16,7 @@ namespace ReservationManagment
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ResMngView());
+            Application.Run(new ReservationCreateView());
         }
     }
 }
